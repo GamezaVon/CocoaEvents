1bfb2717fbb6c2a215b2f6ff68b16697df62ff1f
GamezaVon <acocolito@gmail.com>
2021-08-09 10:37:37 -0700

    Fixed image to URL; added world-flags and individual asset paths, along with assets.debug true and assets.check_precompiled_asset false

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-ruby '2.6.3'
+ruby '3.0.2'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 6.0.3', '>= 6.0.3.2'
@@ -17,6 +17,9 @@ gem 'webpacker', '~> 4.0'
 gem 'turbolinks', '~> 5'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.7'
+# Fixing Image into URL
+gem 'world-flags', github: 'kristianmandrup/world-flags', branch: 'master'
+gem 'wdm', '>= 0.1.0' if Gem.win_platform?
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,9 +7,14 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
-# Rails.application.config.assets.paths << Rails.root.join('vendor/*')
+Rails.application.config.assets.paths << Rails.root.join('vendor/assets/bootstrap/js')
+Rails.application.config.assets.paths << Rails.root.join('vendor/assets/glightbox/js')
+Rails.application.config.assets.paths << Rails.root.join('vendor/assets/isotope-layout/')
+Rails.application.config.assets.paths << Rails.root.join('vendor/assets/php-email-form/')
+Rails.application.config.assets.paths << Rails.root.join('vendor/assets/purecounter/')
+Rails.application.config.assets.paths << Rails.root.join('vendor/assets/swiper/')
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-Rails.application.config.assets.precompile += [Rails.root.join('vendor/*')]
+Rails.application.config.assets.precompile += %w('*.js .bundle.min.js')
